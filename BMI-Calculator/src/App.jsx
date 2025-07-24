7f553743ea283e5cce05d4a2d07649c2726b0882
Rupeshprajapati421 <rupeshdaksh01@gmail.com>
2025-07-24 13:38:15 +0530

    save

--- BMI-Calculator/src/App.jsx
+++ BMI-Calculator/src/App.jsx
@@ -46,7 +46,7 @@ function App() {
     <>
       <div className="app ">
       <h1>hello world </h1>
-      <h1>this is rupesh thisiis mycall </h1>
+      <h1>this is rupesh thighjklhfjghklgksiis mycall </h1>
         <div className="container ">
           <h1>BMI CALCULATOR</h1>
           <form action="" onSubmit={calBmi}>
